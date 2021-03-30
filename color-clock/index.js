fef1850081787c3eec8e06fd0c7ffa64ade4631f
prahal <vprahaladhan@gmail.com>
2021-03-30 11:59:50 +1100

    initial commit

--- color-clock/index.js
+++ color-clock/index.js
@@ -1,5 +1,7 @@
 //No need to do any work here.  
 //If package.json and the proper modules are installed in the correct location, this should display correctly in the browser.
+// import moment from 'moment';
+
 
 function displayTime() {
   let clock = document.querySelector('#clock')
